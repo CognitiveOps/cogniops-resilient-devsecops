56997f836d035d5f609cd000e7190d9769cd5737
Yanna Koutroumpi <ykoutroum@gmail.com>
2025-10-26 20:06:44 +0200

    fix: update infra for bucket location var and WIF pool admin role

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "project_id" {}
-variable "region" { default = "europe-west1" }
-variable "repo_location" { default = "europe" } # Artifact Registry multi-region
-variable "github_repo" {} # "org/repo"
-variable "bucket_location" { default = "EU" } # ή "europe-west1"
+variable "region"        { default = "europe-west1" }
+variable "repo_location" { default = "europe" }     # για Artifact Registry
+variable "github_repo"   {}                         # "org/repo"
+variable "bucket_location" { default = "EU" }       # GCS: "EU" (multi-region) ή π.χ. "europe-west1"
